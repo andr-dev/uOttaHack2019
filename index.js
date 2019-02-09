dd346b7db39a41a4408e834afac1e4702f66bc7c
andr-dev <andrebsec@gmail.com>
2019-02-09 14:06:28 -0500

    Basic Node.JS Authentication and Routing

--- index.js
+++ index.js
@@ -9,36 +9,40 @@ var MongoStore = require('connect-mongo')(session);
 
 const _path = __dirname + '/public/';
 
-// mongoose.connect('', {useCreateIndex: true, useNewUrlParser: true});
-// var db = mongoose.connection;
-// mongoose.set('useFindAndModify', false);
+// mongoose.connect('mongodb+srv://andr-dev:9Pqhz5U3TiyS6eVX@business-webapp-ooehl.gcp.mongodb.net/test?retryWrites=true', {useCreateIndex: true, useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/budgetbuddy', {useCreateIndex: true, useNewUrlParser: true});
+var db = mongoose.connection;
+mongoose.set('useFindAndModify', false);
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//     console.log('Mongoose Connected');
-//     db.dropDatabase(function () {
-//       console.log('Reset MongoDB Database');
-//     });
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Mongoose Connected');
+    db.dropDatabase(function () {
+        console.log('Reset MongoDB Database');
+        setupExpress();
+    });
+});
 
 app.use(session({
     secret: 'abcde12345_idk_uOttaHack2019',
     resave: true,
     saveUninitialized: false,
-    // store: new MongoStore({
-    //     mongooseConnection: db
-    // })
+    store: new MongoStore({
+        mongooseConnection: db
+    })
 }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+function setupExpress () {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(_path));
-const routes = require('./routes.js');
-app.use('/', routes);
+    app.use(express.static(_path));
+    const routes = require('./routes.js');
+    app.use('/', routes);
 
-var port = 3000;
+    var port = 3000;
 
-app.listen(port, function () {
-    console.log('Server started on port: ' + port);
-})
+    app.listen(port, function () {
+        console.log('Server started on port: ' + port);
+    });
+}
